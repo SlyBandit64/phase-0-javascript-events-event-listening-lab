9028a8dfc064fe278855245dc2ed935852b11079
Scott Taylor <rscott.taylor01@gmail.com>
2023-04-16 15:32:14 -0500

    Completes Lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clickled! The horror!!');
+}
+
+input.addEventListener('click', clickAlert);
 }
